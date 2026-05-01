6d157a201e9dca8eefc32107bab95e325964761e
WarLikeLaux <warlikelaux080399@mail.ru>
2026-05-01 14:30:33 +0600

    #1 - removed Fortify and auth scaffolding

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,16 @@
 <?php
 
+use App\Http\Controllers\FlashcardController;
+use App\Http\Controllers\StudyController;
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Features;
 
-Route::inertia('/', 'welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
+Route::redirect('/', '/flashcards');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'dashboard')->name('dashboard');
-});
+Route::get('flashcards', [FlashcardController::class, 'index'])->name('flashcards.index');
+Route::get('flashcards/create', [FlashcardController::class, 'create'])->name('flashcards.create');
+Route::post('flashcards', [FlashcardController::class, 'store'])->name('flashcards.store');
+Route::delete('flashcards/{flashcard}', [FlashcardController::class, 'destroy'])->name('flashcards.destroy');
+Route::post('flashcards/reset', [FlashcardController::class, 'reset'])->name('flashcards.reset');
 
-require __DIR__.'/settings.php';
+Route::get('study', [StudyController::class, 'show'])->name('study.show');
+Route::post('study/{flashcard}/answer', [StudyController::class, 'answer'])->name('study.answer');
